e0a2a6ac47fb5cd2db707baa3b6eb08e97e7d38a
yang.yuncai <383235388@qq.com>
2018-05-30 11:11:50 +0800

    chore: 优化开发服务启动配置。

---
+++ serve.config.js
@@ -0,0 +1,18 @@
+/**
+ * @description - webpack-server configurations
+ * @author - huang.jian <hjj491229492@hotmail.com>
+ */
+
+/* eslint-disable import/no-extraneous-dependencies */
+const convert = require('koa-connect');
+const history = require('connect-history-api-fallback');
+const proxy = require('http-proxy-middleware');
+
+module.exports = {
+  add: (app) => {
+    app.use(
+      convert(proxy('/story', { target: 'https://story.babycherry.cn' }))
+    );
+    app.use(convert(history()));
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,38 @@
 
 // dependency
 const presets = require('@coco-platform/webpack-preset');
-const convert = require('koa-connect');
-const history = require('connect-history-api-fallback');
-const proxy = require('http-proxy-middleware');
+const _ = require('lodash');
+const request = require('superagent');
 
 // scope
+const address =
+  // eslint-disable-next-line max-len
+  'https://gist.githubusercontent.com/huang-xiao-jian/a66ff80a5c811066dccb11811b734a49/raw/7e72c26a60158c5d13bf5f59c86881b3d07ddc08/bootcdn.externals.stable.yml';
 const options = {
-  entry: 'src/main.jsx',
-};
-
-module.exports = presets.development(options);
-module.exports.serve = {
-  add: (app) => {
-    app.use(
-      convert(proxy('/story', { target: 'https://story.babycherry.cn' }))
-    );
-    app.use(convert(history()));
+  development: {
+    entry: 'src/main.jsx',
   },
 };
+const externals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+};
+
+module.exports = new Promise((resolve, reject) => {
+  request
+    .get(address)
+    .timeout(12000)
+    .then((res) => res.text)
+    .then((definition) => {
+      const developmentOptions = _.assign({}, options.development, {
+        definition,
+      });
+      const developmentConfiguration = presets.development(developmentOptions);
+      const configuration = _.assign({}, developmentConfiguration, {
+        externals,
+      });
+
+      resolve(configuration);
+    })
+    .catch((err) => reject(err));
+});
