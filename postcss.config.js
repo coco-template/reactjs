8d99f16fbd77bb1ddb8822fb8bc097f0ac7933fe
yang.yuncai <383235388@qq.com>
2018-09-25 15:25:37 +0800

    chore: split postcss into project.

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+/**
+ * @description - postcss options
+ * @author - huang.jian <hjj491229492@hotmail.com>
+ */
+
+/* eslint-disable import/no-extraneous-dependencies */
+const autoprefixer = require('autoprefixer');
+const nested = require('postcss-nested');
+const flexbugs = require('postcss-flexbugs-fixes');
+
+module.exports = {
+  plugins: [
+    flexbugs(),
+    nested(),
+    autoprefixer({
+      browsers: [
+        '>1%',
+        'last 4 versions',
+        'Firefox ESR',
+        'not ie <= 9', // React doesn't support IE8 anyway
+      ],
+    }),
+  ],
+};
