1e9126f4efadc5f92e826c5b52f93d344b6e3c87
huang.jian <hjj491229492@hotmail.com>
2018-10-31 14:06:31 +0800

    chore: unify jest configuration.

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,19 @@
 module.exports = {
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '\\.(ts|tsx|js|jsx)$': 'babel-jest',
     '\\.(jpg|jpeg|png|giff|webp|svg)$':
       '@coco-platform/jest-tools/lib/asset-transformer.js',
   },
-  moduleFileExtensions: ['js', 'jsx', 'json'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
   moduleNameMapper: {
     '\\.css$': '@coco-platform/jest-tools/lib/css-ignore.js',
     '\\.pcss': '@coco-platform/jest-tools/lib/css-modules.js',
   },
-  // Coverage report
-  collectCoverageFrom: ['src/**/*.js', 'src/**/*.jsx'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'html'],
-  // Test configuration
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\]'],
-  // Coverage report
-  // Test configuration
-  testEnvironment: 'node',
-  testMatch: [
-    '<rootDir>/__tests__/**/*.spec.js',
-    '<rootDir>/__tests__/**/*.spec.jsx',
-  ],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/es/'],
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/__tests__/**/*.spec.{js,jsx,ts,tsx}'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
