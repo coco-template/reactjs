0ed37432aa3c9afb58c25efe313e2384ce52c273
huang.jian <hjj491229492@hotmail.com>
2019-07-19 14:37:54 +0800

    fix(webpack.production.js): adapt css loader options change :bug:
    fix #4

--- webpack.production.js
+++ webpack.production.js
@@ -73,9 +73,10 @@ module.exports = {
           {
             loader: require.resolve('css-loader'),
             options: {
-              modules: true,
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
               importLoaders: 1,
-              localIdentName: '[name]__[local]__[hash:base64:5]',
             },
           },
           {
