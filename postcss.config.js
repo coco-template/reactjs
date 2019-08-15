f5191d7b9fd9811ce2af4546b8faec3450ad08f8
huang.jian <hjj491229492@hotmail.com>
2019-08-15 15:31:33 +0800

    chore: change postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,29 @@
  */
 
 /* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
 const autoprefixer = require('autoprefixer');
 const nested = require('postcss-nested');
 const flexbugs = require('postcss-flexbugs-fixes');
+const atImport = require('postcss-import');
+const atUrl = require('postcss-url');
+const PXToViewport = require('postcss-px-to-viewport');
 
 module.exports = {
-  plugins: [flexbugs(), nested(), autoprefixer()],
+  plugins: [
+    atImport(),
+    atUrl({
+      basePath: path.resolve(__dirname, 'src'),
+    }),
+    flexbugs(),
+    nested(),
+    autoprefixer(),
+    /* use self own dimension */
+    PXToViewport({
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 5,
+      selectorBlackList: [],
+    }),
+  ],
 };
